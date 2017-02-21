27d61eed6985784fa438b65b2fbbbbd7714b47d8
Varun Rajan <varunrajan@Varuns-MacBook-Pro.local>
2017-02-20 20:22:19 -0500

    Overhauled geocoding, added Yelp scraper

--- geocoding.py
+++ geocoding.py
@@ -1,12 +1,12 @@
 
 def geocoding_from_location(location, delay=0.05):
     # lat, long are in parentheses at end of string
-    coords = location.rpartition('(')[2].partition(')')
+    coords = location.rpartition('(')[2].partition(')')[0]
 
     def post_process_coord(coord):
         coord = coord.strip()
-        coord = int(coord)
+        coord = float(coord)
         return coord
 
-    latitude, longitude = coords.split(',').map(post_process_coord)
+    latitude, longitude = map(post_process_coord, coords.split(','))
     return latitude, longitude
---
+++ yelp_fusion_api.py
@@ -0,0 +1,107 @@
+import requests
+import numpy as np
+import yaml
+import time
+from urllib.parse import quote
+from urllib.parse import urlencode
+
+
+# OAuth credential placeholders that must be filled in by users.
+# You can find them on
+# https://www.yelp.com/developers/v3/manage_app
+CONFIG = yaml.load(open('config.yaml', 'r'))['yelp']
+CLIENT_ID = CONFIG['CLIENT_ID']
+CLIENT_SECRET = CONFIG['CLIENT_SECRET']
+
+
+# API constants, you shouldn't have to change these.
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+TOKEN_PATH = '/oauth2/token'
+GRANT_TYPE = 'client_credentials'
+
+
+def obtain_bearer_token(host, path):
+    """Given a bearer token, send a GET request to the API.
+
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        url_params (dict): An optional set of query parameters in the request.
+    Returns:
+        str: OAuth bearer token, obtained using client_id and client_secret.
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    assert CLIENT_ID, "Please supply your client_id."
+    assert CLIENT_SECRET, "Please supply your client_secret."
+    data = urlencode({
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'grant_type': GRANT_TYPE,
+    })
+    headers = {
+        'content-type': 'application/x-www-form-urlencoded',
+    }
+    response = requests.request('POST', url, data=data, headers=headers)
+    bearer_token = response.json()['access_token']
+    return bearer_token
+
+
+def request(host, path, bearer_token, url_params=None, verbose=False):
+    """Given a bearer token, send a GET request to the API.
+
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        bearer_token (str): OAuth bearer token, obtained using client_id and client_secret.
+        url_params (dict): An optional set of query parameters in the request.
+    Returns:
+        dict: The JSON response from the request.
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
+    url_params = url_params or {}
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    headers = {
+        'Authorization': 'Bearer %s' % bearer_token,
+    }
+
+    if verbose:
+        print('Querying {0} ...'.format(url))
+
+    response = requests.request('GET', url, headers=headers, params=url_params)
+
+    return response.json()
+
+
+def search_by_zipcode(bearer_token, zipcode,
+                      term='Grocery', page=0,
+                      results_per_page=50):
+    url_params = {
+        'term': term,
+        'location': str(zipcode),
+        'limit': results_per_page,
+        'offset': page * results_per_page
+    }
+    return request(API_HOST, SEARCH_PATH, bearer_token, url_params=url_params)
+
+
+def emit_all_by_zipcode(bearer_token, zipcode,
+                        term='Grocery', max_results=150,
+                        results_per_page=50, delay=1):
+    pages = int(np.ceil(max_results / results_per_page))
+    for page in range(pages):
+        res = search_by_zipcode(bearer_token, zipcode, term,
+                                page, results_per_page)
+        time.sleep(delay)  # let's keep Yelp happy
+        yield res
+
+
+def emit_all(bearer_token, zipcodes,
+             term='Grocery', max_results=150,
+             results_per_page=50, delay=1):
+    for zipcode in zipcodes:
+        yield from emit_all_by_zipcode(bearer_token, zipcode, term,
+                                       max_results, results_per_page, delay)
